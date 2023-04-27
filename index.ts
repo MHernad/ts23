fa97f8aaf8d2e5426f8132ee1913ece18cdce103
MHernad <marcoshernad23@gmail.com>
2023-04-27 13:46:54 -0300

    swaggen't

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import express from "express"
 import swaggerUi from 'swagger-ui-express';
-import * as swaggerDocument from "./swagger";
+//import * as swaggerDocument from "./swagger";
 import { conexion } from './database/dbConnection';
 import { rutasAlumnos } from "./rutas/rutasAlumnos";
 import { rutasMaterias } from "./rutas/rutasMaterias";
@@ -10,7 +10,7 @@ import { rutasPersonalizadas } from "./rutas/rutasPersonalizadas";
 const app: express.Application = express();
 
 app.use(express.json());
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+//app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 app.get('/', (_req , _res) => _res.send('Bienvenido a mi API REST!'));
 
